67952f96017affb0901b057ce2d50108b563a07f
horsewin <mamamatz@gmail.com>
2020-01-10 14:33:53 +0900

    Create test.sh

---
+++ scripts/test.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -ex
+
+# バージョン情報の取得と生成
+version=$(cat ./package.json | jq -r .version)
+
+major=$(echo ${version} | cut -d . -f 1)
+minor=$(echo ${version} | cut -d . -f 2)
+patch=$(echo ${version} | cut -d . -f 3 | cut -d - -f 1)
+build=$(echo ${version} | cut -d d -f 2)
+
+if [ $1 = "prod" ]; then
+  minor=`expr ${minor} + 1`
+  patch=0
+else
+  patch=`expr ${patch} + 1`
+fi
+
+NEW_VERSION_NAME="${major}.${minor}.${patch}"
+
+echo ${NEW_VERSION_NAME}
